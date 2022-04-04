f8b43b7bfbbe61267c90dbce62c8eab59f58b1d2
Илья Бондарчук <type1251@outlook.com>
2022-04-04 18:55:17 +1200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstNumber = new System.Windows.Forms.TextBox();
+            this.SecondNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Plus_button = new System.Windows.Forms.Button();
+            this.Minus_button = new System.Windows.Forms.Button();
+            this.Mul_button = new System.Windows.Forms.Button();
+            this.Div_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FirstNumber
+            // 
+            this.FirstNumber.Location = new System.Drawing.Point(35, 70);
+            this.FirstNumber.Name = "FirstNumber";
+            this.FirstNumber.Size = new System.Drawing.Size(125, 20);
+            this.FirstNumber.TabIndex = 0;
+            this.FirstNumber.Click += new System.EventHandler(this.FirstNumber_Click);
+            this.FirstNumber.TextChanged += new System.EventHandler(this.FirstNumber_TextChanged);
+            // 
+            // SecondNumber
+            // 
+            this.SecondNumber.Location = new System.Drawing.Point(309, 70);
+            this.SecondNumber.Name = "SecondNumber";
+            this.SecondNumber.Size = new System.Drawing.Size(125, 20);
+            this.SecondNumber.TabIndex = 1;
+            this.SecondNumber.Click += new System.EventHandler(this.SecondNumber_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(32, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Первое число";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(306, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Второе число";
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(178, 172);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(141, 20);
+            this.Result.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(194, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Результат";
+            // 
+            // Plus_button
+            // 
+            this.Plus_button.Location = new System.Drawing.Point(198, 48);
+            this.Plus_button.Name = "Plus_button";
+            this.Plus_button.Size = new System.Drawing.Size(34, 23);
+            this.Plus_button.TabIndex = 6;
+            this.Plus_button.Text = "+";
+            this.Plus_button.UseVisualStyleBackColor = true;
+            this.Plus_button.Click += new System.EventHandler(this.Plus_button_Click);
+            // 
+            // Minus_button
+            // 
+            this.Minus_button.Location = new System.Drawing.Point(250, 48);
+            this.Minus_button.Name = "Minus_button";
+            this.Minus_button.Size = new System.Drawing.Size(34, 23);
+            this.Minus_button.TabIndex = 7;
+            this.Minus_button.Text = "-";
+            this.Minus_button.UseVisualStyleBackColor = true;
+            this.Minus_button.Click += new System.EventHandler(this.Minus_button_Click);
+            // 
+            // Mul_button
+            // 
+            this.Mul_button.Location = new System.Drawing.Point(198, 89);
+            this.Mul_button.Name = "Mul_button";
+            this.Mul_button.Size = new System.Drawing.Size(34, 23);
+            this.Mul_button.TabIndex = 8;
+            this.Mul_button.Text = "*";
+            this.Mul_button.UseVisualStyleBackColor = true;
+            this.Mul_button.Click += new System.EventHandler(this.Mul_button_Click);
+            // 
+            // Div_button
+            // 
+            this.Div_button.Location = new System.Drawing.Point(250, 89);
+            this.Div_button.Name = "Div_button";
+            this.Div_button.Size = new System.Drawing.Size(34, 23);
+            this.Div_button.TabIndex = 9;
+            this.Div_button.Text = "/";
+            this.Div_button.UseVisualStyleBackColor = true;
+            this.Div_button.Click += new System.EventHandler(this.Div_button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_clear);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(471, 249);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Div_button);
+            this.Controls.Add(this.Mul_button);
+            this.Controls.Add(this.Minus_button);
+            this.Controls.Add(this.Plus_button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SecondNumber);
+            this.Controls.Add(this.FirstNumber);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Калькулятор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstNumber;
+        private System.Windows.Forms.TextBox SecondNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Plus_button;
+        private System.Windows.Forms.Button Minus_button;
+        private System.Windows.Forms.Button Mul_button;
+        private System.Windows.Forms.Button Div_button;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    
+    public partial class Form1 : Form
+    {
+        bool FirstWasErased = false;
+        bool SecondWasErased = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            FirstNumber.Text = "123...";
+            SecondNumber.Text = "456...";
+        }
+
+
+        private void Result_Function(int choice)
+        {
+            try
+            {
+                string first_n_t = FirstNumber.Text;
+                string second_n_t = SecondNumber.Text;
+
+                double first_n = Convert.ToDouble(first_n_t);
+                double second_n = Convert.ToDouble(second_n_t);
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Result.Text = Convert.ToString(first_n + second_n);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Result.Text = Convert.ToString(first_n - second_n);
+                            break;
+                        }
+                    case 3:
+                        {
+                            Result.Text = Convert.ToString(first_n * second_n);
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (second_n == 0)
+                            {
+                                MessageBox.Show("На ноль делить нельзя", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                                break;
+                            }
+                            Result.Text = Convert.ToString(first_n / second_n);
+                            break;
+                        }
+                };
+            }
+            catch (FormatException) {
+                MessageBox.Show("В полях должны быть цифры", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            };
+            
+        }
+        private void Plus_button_Click(object sender, EventArgs e)
+        {
+            Result_Function(1);
+        }
+
+        private void Button_clear(object sender, EventArgs e)
+        {
+            FirstNumber.Text = "";
+            SecondNumber.Text = "";
+            Result.Text = "";
+        }
+
+        private void Minus_button_Click(object sender, EventArgs e)
+        {
+            Result_Function(2);
+        }
+
+        private void Mul_button_Click(object sender, EventArgs e)
+        {
+            Result_Function(3);
+        }
+
+        private void Div_button_Click(object sender, EventArgs e)
+        {
+            Result_Function(4);
+        }
+
+        private void FirstNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FirstNumber_Click(object sender, EventArgs e)
+        {
+            if (FirstWasErased == false)
+            {
+                FirstNumber.Text = "";
+                FirstWasErased = true;
+            }
+        }
+
+        private void SecondNumber_Click(object sender, EventArgs e)
+        {
+            if (SecondWasErased == false)
+            {
+                SecondNumber.Text = "";
+                SecondWasErased = false;
+            }
+        }
+    }
+}
